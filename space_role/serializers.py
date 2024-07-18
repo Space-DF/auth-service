750a49e22e3411cabbce69cf919510b7bbd7cb99
pikann <hvhai22@gmail.com>
2024-07-18 14:26:31 +0700

    fix: fix error cannot create space role

--- space_role/serializers.py
+++ space_role/serializers.py
@@ -19,6 +19,7 @@ class SpaceRoleSerializer(serializers.ModelSerializer):
         fields = "__all__"
         extra_kwargs = {
             "id": {"read_only": True},
+            "space": {"read_only": True},
             "created_at": {"read_only": True},
             "updated_at": {"read_only": True},
         }
