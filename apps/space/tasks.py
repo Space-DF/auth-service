d6156e00db74682bc9b5f1a049cd9c6ac8eb5275
ngovinh2k2 <vinh.ngo@digitalfortress.dev>
2025-10-15 14:57:06 +0700

    fix: total devices not work

--- apps/space/tasks.py
+++ apps/space/tasks.py
@@ -4,7 +4,8 @@ from common.apps.space.models import Space
 from common.celery import constants
 from common.celery.tasks import task
 from django.db import transaction
-from django.db.models import Case, F, When
+from django.db.models import BooleanField, Case, F, IntegerField, Value, When
+from django.db.models.functions import Greatest
 from django.db.utils import ProgrammingError
 from django_tenants.utils import schema_context
 
@@ -13,6 +14,8 @@ from django_tenants.utils import schema_context
 @task(
     name=f"spacedf.tasks.{constants.AUTH_SERVICE_ADD_OR_REMOVE_DEVICE}",
     autoretry_for=(ProgrammingError,),
+    retry_backoff=2,
+    max_retries=3,
 )
 @transaction.atomic
 def add_or_remove_device(**kwargs):
@@ -20,10 +23,18 @@ def add_or_remove_device(**kwargs):
         "slug_name", "space_slug_name", "type"
     )(kwargs)
     with schema_context(slug_name):
+        value = Case(
+            When(
+                Value(action_type == "add", output_field=BooleanField()), then=Value(1)
+            ),
+            When(
+                Value(action_type == "remove", output_field=BooleanField()),
+                then=Value(-1),
+            ),
+            default=Value(0),
+            output_field=IntegerField(),
+        )
+
         Space.objects.filter(slug_name=space_slug_name).update(
-            total_devices=Case(
-                When(condition=(action_type == "add"), then=F("total_devices") + 1),
-                When(condition=(action_type == "remove"), then=F("total_devices") - 1),
-                default=F("total_devices"),
-            )
+            total_devices=Greatest(F("total_devices") + value, Value(0)),
         )
